6b82358302d22da5ee07088955218313cd2f00a7
pradhul_kannan <codedream145@gmail.com>
2023-04-07 09:07:52 +0530

    eating process_0.7

--- static/jscript.js
+++ static/jscript.js
@@ -1,1 +1,6 @@
-document.getElementById("")
+function alert_fun (menu_id) {
+    menu = document.getElementById(menu_id+"d");
+    menubutton = document.getElementById(menu_id);
+    menu.style.textDecoration = "line-through";
+    menubutton.classList.add("disabled");
+}
