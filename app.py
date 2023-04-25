a1291df505ae00c24b666708329d87f2e6d37d3f
pradhul_kannan <codedream145@gmail.com>
2023-04-25 15:26:01 +0530

    version 1.0 - ready to go

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, request, session, flash
 from cs50 import SQL
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from helpers import login_required, apology
 
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 app.secret_key = "secret key buddy"
 
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=17)
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
